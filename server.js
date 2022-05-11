01eea6adc661803aae5b2263d056cc59246f6b4e
Shintaro Wakayama <shintarowakayama@hotmail.com>
2022-05-11 09:54:52 +0900

    ブラシの色を反映するようにした。

--- server.js
+++ server.js
@@ -1,43 +1,40 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
 // Webサーバーを起動
-var server = app.listen(process.env.PORT || 3000, listen);
+let server = app.listen(process.env.PORT || 3000, listen);
 
 // サーバーが起動したときに呼び出される
-function listen(){
+var listen = () => {
   var host = server.address().adress;
   var port = server.address().port;
   console.log(`http://${host}:${port}`);
 }
 
-var io = require('socket.io')(server, {
+let io = require('socket.io')(server, {
   cors: {
-    origins: ["http://localhost:8080"],
-    methods: ["GET", "POST"]
+    origins: ['http://localhost:8080', 'http://vue-painter.s3-website-ap-northeast-1.amazonaws.com/'],
+    methods: ['GET', 'POST']
   }
 });
 
 // クライアントに接続されたときの処理を行う
 io.sockets.on('connection',
-  function(socket){
+  (socket) => {
 
-    console.log("socket.id: " + socket.id);
+    console.log('socket.id: ' + socket.id);
 
     //クライアント側のstrokeイベントを受け取る
     socket.on('stroke',
 
-      function(strokes){
+      (strokes, color) => {
         console.log(`Stroke length: ${strokes.length}`)
         if (strokes.length > 0) {
           console.log('stroke.pointerId:', strokes[0])
         }
-        // strokes.forEach(stroke => {
-        //   console.log(`stroke: ${stroke.x}, ${stroke.y}`)
-        // });
 
         //ブロードキャストで全員に向けて送信する
-        socket.broadcast.emit('stroke', strokes);
+        socket.broadcast.emit('stroke', strokes, color);
       }
     );
 
